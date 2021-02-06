96a565fdf3f99d0af4164c053057518fb6b5bb07
Thiago Amorim <ttthiago.amorim@gmail.com>
2021-02-06 01:34:39 -0300

    Correção da View Index da tela REGISTRE-SE

--- Controllers/RegistroController.cs
+++ Controllers/RegistroController.cs
@@ -1,151 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Models;
-
 
 namespace SalesWebMvc.Controllers
 {
     public class RegistroController : Controller
     {
-        private readonly Contexto _context;
-
-        public RegistroController(Contexto context)
-        {
-            _context = context;
-        }
-
-        // GET: Usuarios
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Registro.ToListAsync());
-        }
-
-        // GET: Usuarios/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var registro = await _context.Registro
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (registro == null)
-            {
-                return NotFound();
-            }
-
-            return View(registro);
-        }
-
-        // GET: Usuarios/Create
-        public IActionResult Create()
+        public IActionResult Index()
         {
             return View();
-        }
-
-        // POST: Usuarios/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Celular,Email")] Registro registro)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(registro);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(registro);
-        }
-
-        // GET: Usuarios/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var registro = await _context.Registro.FindAsync(id);
-            if (registro == null)
-            {
-                return NotFound();
-            }
-            return View(registro);
-        }
-
-        // POST: Usuarios/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Celular,Email")] Registro registro)
-        {
-            if (id != registro.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(registro);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UsuarioExists(registro.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(registro);
-        }
-
-        // GET: Usuarios/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var usuario = await _context.Registro
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (usuario == null)
-            {
-                return NotFound();
-            }
-
-            return View(usuario);
-        }
-
-        // POST: Usuarios/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var usuario = await _context.Registro.FindAsync(id);
-            _context.Registro.Remove(usuario);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool UsuarioExists(int id)
-        {
-            return _context.Registro.Any(e => e.Id == id);
         }
     }
 }
--- Services/RegistroService.cs
+++ Services/RegistroService.cs
@@ -3,9 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMvc.Models;
 
 
-namespace SalesWebMvc.Services
+namespace SalesWebMvc.Data
 {
     public class RegistroService
     {
